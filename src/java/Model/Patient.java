fcd09149ed54108c5b90514fc293612964c18aaa
Neal Valdez <nealvaldez10@hotmail.com>
2020-04-11 21:19:05 -0400

    Patient select,insert,delete methods added

--- src/java/Model/Patient.java
+++ src/java/Model/Patient.java
@@ -5,10 +5,176 @@
  */
 package Model;
 
+import Model.DBAccess.Access;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Neal Valdez
  */
-public class Patient {
+public class Patient extends Person{
+    String address;
+    String insCo;
     
+    public Patient(){
+    this("","","","","","","");
 }
+
+    public Patient(String Id,String password, String fname, String lname, String address, String email, String insCo){
+        super(Id,password,fname,lname,email);
+        
+        this.address = address;
+        this.insCo = insCo;
+        
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+    
+    public String getInsCo(){
+        return insCo;
+    }
+    
+    public void setInsCo(String insCo){
+        this.insCo = insCo;
+    }
+    
+    public void selectDB(String Id){
+        try{
+            Access databaseAccess = new Access();
+            
+            //execute statment
+            String sql = "select * from Patients WHERE patId = '" + Id + "'";             
+            ResultSet result = databaseAccess.getStatement().executeQuery(sql);            
+
+            result.next();
+            
+            //set properties
+            setId(result.getString(1)); 
+            setPw(result.getString(2));
+            setFname(result.getString(3)); 
+            setLname(result.getString(4));                                               
+            setAddress(result.getString(5));
+            setEmail(result.getString(6));
+            setInsCo(result.getString(7));
+            
+            System.out.println("Patient " + Id + " Successfully selected" + System.lineSeparator());
+            
+            databaseAccess.close();            
+        } 
+        catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Exception caught - " + ex + System.lineSeparator());
+        }
+    }
+    
+    public void insertDB(){
+        try {
+             Access databaseAccess = new Access();
+            
+            //setup statment
+            String sql = "INSERT INTO Patients " +
+                         "VALUES('" +getId()+ "', '" + getPw()+ "', '" + getFname()+ "', '" + getLname()+ "', '" + getAddress()+ "', '" + getEmail()+ "', '" + getInsCo()+ "')";             
+            
+            //execute insertion                         
+            int num = databaseAccess.getStatement().executeUpdate(sql);
+            
+            //deal with result
+            if (num == 1){
+                
+                //debug to console
+                System.out.println("Insert successful!" + System.lineSeparator());
+                
+            }else {
+                //debug to console
+                System.out.println("Insert failed!" + System.lineSeparator());
+            }
+            
+            databaseAccess.close();
+
+        }
+
+        catch (ClassNotFoundException | SQLException e){
+            System.out.println("Exception caught - " + e + System.lineSeparator());
+        }
+    }
+    
+    public void deleteDB(){
+        try{
+            Access databaseAccess = new Access();
+            
+            //setup statment
+            String sql = "DELETE FROM Patients WHERE patId = " + "'"+getId()+"'";          
+            System.out.println(sql);
+            //execute Deletion                                               
+            int num = databaseAccess.getStatement().executeUpdate(sql);
+            
+            //deal with result
+            if (num == 1){
+                //debug to console
+                System.out.println("Deletion successful!" + System.lineSeparator());
+                //reset properties to blank values
+                this.setId("");
+                this.setPw("");
+                this.setFname("");
+                this.setLname("");
+                this.setAddress("");
+                this.setEmail("");
+                this.setInsCo("");
+            }else {
+                //debug to console
+                System.out.println("Deletion failed!" + System.lineSeparator());
+            }                        
+        } 
+        catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Exception caught - " + ex + System.lineSeparator());
+        }
+    }
+    
+    public void display(){
+       System.out.println("Id: "+getId()+"\t Password: "+getPw()+"\t Fname: "+
+                            getFname()+"\t Lname: "+getLname()+"\t Lname: "+getAddress()+"\t email: "+getEmail()+"\t InsCo: "+getInsCo());
+    }
+    
+    
+    
+    public static void main(String[]args){
+        Patient p1 = new Patient();
+        p1.selectDB("A900");
+        p1.display();
+        
+        Patient p2 = new Patient();
+        p2.selectDB("A912");
+        p2.deleteDB();
+        
+    }
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
